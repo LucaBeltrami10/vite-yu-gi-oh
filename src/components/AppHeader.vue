89276f2c1f588b2585c56e6da98f885d3f628a24
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-18 17:46:13 +0100

    inserito scss in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,7 @@
 <template>
     <header>
-        <img src="https://e7.pngegg.com/pngimages/534/999/png-clipart-yu-gi-oh-power-of-chaos-yugi-the-destiny-yu-gi-oh-the-sacred-cards-yugi-mutou-bakura-yu-miscellaneous-game.png"
-            alt="yu-gi-oh logo">
-        <h1>Yu-Gi-Oh Api</h1>
+        <img class="ms-4" src="https://upload.wikimedia.org/wikipedia/commons/2/21/Yu-Gi-Oh%21.png" alt="yu-gi-oh logo">
+        <h1 class="ms-4">Yu-Gi-Oh Api</h1>
     </header>
 </template>
 <script>
@@ -11,8 +10,12 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as *;
+
 header {
-    height: 100px;
+    @include flex(row, start, center);
+    height: 80px;
 
     img {
         height: 100%;
