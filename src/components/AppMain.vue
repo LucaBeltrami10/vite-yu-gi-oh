f938219dcdc687bd4dbd71f9dd9c679e7169e2bc
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-18 18:03:36 +0100

    scss main + creato card element e importato in main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,35 @@
-<template lang="">
+<template>
+    <main>
+        <div class="my_container">
+            <CardElement />
+
+        </div>
+    </main>
 </template>
 <script>
+import CardElement from './CardElement.vue';
 export default {
+    components: {
+        CardElement,
+    }
 
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as *;
+
+main {
+    background-color: $background-main;
+    height: 500px;
+    width: 100%;
+    padding: 1rem;
+
+    div.my_container {
+        height: 400px;
+        width: 70%;
+        background-color: white;
+
+    }
+}
+</style>
---
+++ src/components/CardElement.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        ciao
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as *;
+</style>
